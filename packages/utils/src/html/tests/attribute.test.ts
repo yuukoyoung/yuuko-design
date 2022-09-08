cb592da50220d41eb1bb044b6d8654a27746e326
yuuko <yuukoyoung@outlook.com>
2022-09-08 23:48:40 +0800

    test(utils): use vitest

---
+++ packages/utils/src/html/tests/attribute.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest';
+import { JSDOM } from 'jsdom';
+import { hasDisabledAttribute } from '../attribute';
+
+describe('aria', () => {
+  describe('hasDisabledAttribute', () => {
+    test.each([
+      ['button', '<button id="id"></button>', true],
+      ['fieldset', '<fieldset id="id"></fieldset>', true],
+      ['link', '<link id="id" />', true],
+      ['div', '<div id="id"></div>', false],
+    ])('%s', (_name, outerHTML, expected) => {
+      const { window } = new JSDOM(outerHTML);
+      const element = window.document.querySelector('#id');
+
+      expect(hasDisabledAttribute(element as HTMLElement)).toBe(expected);
+    });
+  });
+});
---
+++ packages/utils/src/html/tests/content.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest';
+import { JSDOM } from 'jsdom';
+import { isInteractiveContent } from '../content';
+
+describe('aria', () => {
+  describe('isInteractiveContent', () => {
+    test.each([
+      ['button', '<button id="id"></button>', true],
+      ['a[href]', '<a id="id" href="xxx" />', true],
+      ['div', '<div id="id"></div>', false],
+      ['a', '<a id="id" />', false],
+    ])('%s', (_name, outerHTML, expected) => {
+      const { window } = new JSDOM(outerHTML);
+      const element = window.document.querySelector('#id');
+
+      expect(isInteractiveContent(element as HTMLElement)).toBe(expected);
+    });
+  });
+});
