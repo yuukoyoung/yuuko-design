4c82af084be5abb10fa6078a273f92c916622c56
yuuko <yuukoyoung@outlook.com>
2022-09-09 11:27:49 +0800

    chore(vitest): add environment jsdom

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    environment: 'jsdom',
     coverage: {
       provider: 'c8',
       statements: 90,
