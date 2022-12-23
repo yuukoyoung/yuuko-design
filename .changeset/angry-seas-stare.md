079165bf7234f004670f417a9b0c506d7f87ae18
yuuko <yuukoyoung@outlook.com>
2022-12-23 16:49:09 +0800

    feat(stylelint-config): add configuration

---
+++ .changeset/angry-seas-stare.md
@@ -0,0 +1,5 @@
+---
+"@yuukoyoung/stylelint-config": minor
+---
+
+add configuration
--- packages/stylelint-config/src/scss/index.cjs
+++ packages/stylelint-config/src/scss/index.cjs
@@ -1,18 +1,3 @@
 module.exports = {
   extends: [require.resolve('./lib/scss.cjs')],
-  rules: {
-    'scss/at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: [
-          'tailwind',
-          'layer',
-          'apply',
-          'variants',
-          'responsive',
-          'screen',
-        ],
-      },
-    ],
-  },
 };
